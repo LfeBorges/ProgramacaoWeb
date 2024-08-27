6abc4c58754dca28dcbe19dfc0fa1e089a40f02f
LfeBorges <luiz.bore2004@gmail.com>
2024-08-27 20:32:17 -0300

    Desvios e Condicionais

---
+++ Desvios_Condicionais/ex.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ex_resposta.php" method="POST">
+        <input type="number" name="valor">
+    </form>
+</body>
+</html>
