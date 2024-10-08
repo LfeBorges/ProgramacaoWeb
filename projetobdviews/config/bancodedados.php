527f529278273fb60b30973797e743bf5692f51e
LfeBorges <luiz.bore2004@gmail.com>
2024-10-08 20:39:07 -0300

    atualização exemplo projeto

---
+++ projetobdviews/config/bancodedados.php
@@ -0,0 +1,24 @@
+<?php 
+
+    $host = "localhost";
+    $db = "banco_php";
+    $usuario = "root";
+    $senha = "";
+    $port = "3306";
+
+    try{
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $usuario, $senha); //string de conexão ""
+        if($pdo){
+            echo "Conexão realizada com sucesso!";
+        } else {
+            echo "Erro ao conectar o banco de dados!";
+        }
+
+    } catch(Exception $e){
+        echo "ERRO:". $e->getMessege();
+    }
+    
+
+
+
+?>
